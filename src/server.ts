1e33e478f46aa49f16a93a638925598c8f41740d
Vinicius Marques <vncsmrqs@gmail.com>
2020-10-01 22:37:39 -0300

    Using prettier to style the code

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,30 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
-import bodyParser from "body-parser";
-import {ForecastController} from "@src/controllers/forecast";
-import {Application} from "express";
+import bodyParser from 'body-parser';
+import { ForecastController } from '@src/controllers/forecast';
+import { Application } from 'express';
 
 export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
 
-    constructor(private port = 3000) {
-        super();
-    }
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-    public init():void {
-        this.setupExpress();
-        this.setupControllers();
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
 
-    private setupExpress():void {
-        this.app.use(bodyParser.json())
-    }
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
 
-    private setupControllers():void {
-        const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-        this.addControllers([
-            forecastController
-        ]);
-    }
-
-    public getApp(): Application {
-        return this.app;
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
