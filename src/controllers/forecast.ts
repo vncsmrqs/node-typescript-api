8c8543fb180da80b4917f4498fd86f711bd8adfb
Vinicius Marques <vncsmrqs@gmail.com>
2020-10-01 22:24:23 -0300

    Setuping server and running first test

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,9 +1,12 @@
-describe('Beach forecast functional test', () => {
-    it('should return a forecast with just a few times', async () => {
-        const { body, status } = await global.testRequest.get('/forecast');
+import { Controller, Get } from '@overnightjs/core';
+import {Request, Response} from "express";
 
-        expect(status).toBe(200)
-        expect(body).toEqual([
+@Controller('forecast')
+export class ForecastController {
+
+    @Get('')
+    public getForecastForloggedUser(req: Request, res: Response):void {
+        res.send([
             {
                 "time": "2020-04-26T00:00:00+00:00",
                 "forecast": [{
@@ -38,5 +41,5 @@ describe('Beach forecast functional test', () => {
                 }]
             }
         ]);
-    });
-});
+    }
+}
