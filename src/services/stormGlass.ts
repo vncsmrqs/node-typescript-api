6c66424a3dbc1b17b4ad6c94d0ca940a95eef3e8
Vinicius Marques <vncsmrqs@gmail.com>
2020-10-03 00:50:11 -0300

    Dynamic config with node-config

--- src/services/stormGlass.ts
+++ src/services/stormGlass.ts
@@ -1,5 +1,6 @@
 import { AxiosStatic } from 'axios';
 import { InternalError } from '@src/util/errors/internal-error';
+import config, { IConfig } from 'config';
 
 export interface StormGlassPointSource {
   [key: string]: number;
@@ -46,6 +47,10 @@ export class StormGlassResponseError extends InternalError {
   }
 }
 
+const stormGlassResourceConfig: IConfig = config.get(
+  'App.resources.StormGlass'
+);
+
 export class StormGlass {
   protected request: AxiosStatic;
   readonly stormGlassAPIParams =
@@ -58,11 +63,17 @@ export class StormGlass {
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     try {
+      console.log(stormGlassResourceConfig);
+
       const response = await this.request.get<StormGlassForecastResponse>(
-        `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
+        `${stormGlassResourceConfig.get('apiUrl')}/weather/point/?
+          lat=${lat}
+          &lng=${lng}
+          &params=${this.stormGlassAPIParams}
+          &source=${this.stormGlassAPISource}`,
         {
           headers: {
-            Authorization: 'fake-token',
+            Authorization: stormGlassResourceConfig.get('apiToken'),
           },
         }
       );
